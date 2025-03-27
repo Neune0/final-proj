8ecc5cd71258709e4aed4ffa81dfef8d72e7db00
Raimon <60296186+Neune0@users.noreply.github.com>
2025-03-27 09:09:16 +0100

    setup pagine da fare

---
+++ frontend/src/app/pages/client-profile-page/client-profile-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-profile-page',
+  imports: [],
+  templateUrl: './client-profile-page.component.html',
+  styleUrl: './client-profile-page.component.css'
+})
+export class ClientProfilePageComponent {
+
+}
---
+++ frontend/src/app/pages/professional-profile-page/professional-profile-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professional-profile-page',
+  imports: [],
+  templateUrl: './professional-profile-page.component.html',
+  styleUrl: './professional-profile-page.component.css'
+})
+export class ProfessionalProfilePageComponent {
+
+}
